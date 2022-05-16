84bb0d6254ebc6c2ddf1f6ab66c0a9e5ed705fac
Jared L <48422312+lhjt@users.noreply.github.com>
2022-05-17 04:27:38 +1000

    feat: base tls web implementation

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,9 +1,13 @@
 #![warn(clippy::pedantic)]
 
-use std::env;
+use std::{env, fs::File, io::BufReader};
 
 use actix_web::{web, App, HttpServer};
 use middleware::handle_client_cert;
+use rustls::{
+    server::AllowAnyAnonymousOrAuthenticatedClient, Certificate, PrivateKey, RootCertStore,
+    ServerConfig,
+};
 use tracing::info;
 
 mod middleware;
@@ -11,6 +15,9 @@ mod routes;
 mod tls;
 
 const PORT: u16 = 8080;
+const CA_CERT: &str = "certs/rootCACert.pem";
+const SERVER_CERT: &str = "certs/server-cert.pem";
+const SERVER_KEY: &str = "certs/server-key.pem";
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -22,9 +29,42 @@ async fn main() -> std::io::Result<()> {
 
     info!("Launching SCP proxy version {}", env!("CARGO_PKG_VERSION"));
 
+    let mut cert_store = RootCertStore::empty();
+
+    // import CA cert
+    let ca_cert = &mut BufReader::new(File::open(CA_CERT)?);
+    let ca_cert = Certificate(rustls_pemfile::certs(ca_cert).unwrap()[0].clone());
+
+    cert_store
+        .add(&ca_cert)
+        .expect("root CA not added to store");
+
+    // set up client authentication requirements
+    let client_auth = AllowAnyAnonymousOrAuthenticatedClient::new(cert_store);
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_client_cert_verifier(client_auth);
+
+    // import server cert and key
+    let cert_file = &mut BufReader::new(File::open(SERVER_CERT)?);
+    let key_file = &mut BufReader::new(File::open(SERVER_KEY)?);
+
+    let cert_chain = rustls_pemfile::certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    let mut keys: Vec<PrivateKey> = rustls_pemfile::pkcs8_private_keys(key_file)
+        .unwrap()
+        .into_iter()
+        .map(PrivateKey)
+        .collect();
+    let config = config.with_single_cert(cert_chain, keys.remove(0)).unwrap();
+
     HttpServer::new(|| App::new().default_service(web::route().to(routes::route_whoami)))
         .on_connect(handle_client_cert)
         .bind(("127.0.0.1", PORT))?
+        .bind_rustls(("localhost", 8443), config)?
         .run()
         .await
 }
