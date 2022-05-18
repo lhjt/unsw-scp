4c3d3aec9c44d7ef0805633a81655f88c5ac2a5d
Jared L <48422312+lhjt@users.noreply.github.com>
2022-05-19 06:14:45 +1000

    feat(certman): add `p12` library for `pfx` bundle generation

--- certman/src/lib.rs
+++ certman/src/lib.rs
@@ -1,19 +1,13 @@
 #![warn(clippy::pedantic)]
 
+use anyhow::Context;
 use rcgen::{
-    BasicConstraints,
-    Certificate,
-    CertificateParams,
-    DnType,
-    DnValue,
-    IsCa,
-    KeyPair,
-    SanType,
+    BasicConstraints, Certificate, CertificateParams, DnType, DnValue, IsCa, KeyPair, SanType,
 };
 
 #[derive(Debug, Clone)]
 pub struct ClientCertificatePair {
-    pub key:  String,
+    pub key: String,
     pub cert: String,
 }
 
@@ -30,6 +24,11 @@ pub fn create_ca_certificate() -> anyhow::Result<Certificate> {
         DnValue::Utf8String("Security Challenges Platform".to_string()),
     );
     params.is_ca = IsCa::Ca(BasicConstraints::Unconstrained);
+    params.not_before = std::time::SystemTime::now().into();
+    params.not_after = std::time::SystemTime::now()
+        .checked_add(std::time::Duration::from_secs(60 * 60 * 24 * 365))
+        .context("time addition failed")?
+        .into();
 
     Certificate::from_params(params).map_err(anyhow::Error::msg)
 }
@@ -39,24 +38,35 @@ pub fn create_ca_certificate() -> anyhow::Result<Certificate> {
 /// # Errors
 ///
 /// May error when the supplied CA certificate details, or supplied user parameters are invalid.
-pub fn create_client_cert(
-    ca_cert: &Certificate,
-    ca_keypair: KeyPair,
-    name: String,
-    user_id: String,
-) -> anyhow::Result<ClientCertificatePair> {
+pub fn create_client_cert(name: String, user_id: String) -> anyhow::Result<Certificate> {
     // Create a new key for this certificate
-    let mut params = CertificateParams::from_ca_cert_pem(&ca_cert.serialize_pem()?, ca_keypair)?;
+    let mut params = CertificateParams::new(vec![]);
+    params.not_before = std::time::SystemTime::now().into();
+    params.not_after = std::time::SystemTime::now()
+        .checked_add(std::time::Duration::from_secs(60 * 60 * 24 * 90))
+        .context("time addition failed")?
+        .into();
     params
         .distinguished_name
         .push(DnType::CommonName, DnValue::Utf8String(name));
     params.subject_alt_names.push(SanType::Rfc822Name(user_id));
+    // params
+    //     .subject_alt_names
+    //     .push(SanType::DnsName("localhost".to_string()));
 
-    let cert = Certificate::from_params(params)?;
-    Ok(ClientCertificatePair {
-        key:  cert.serialize_private_key_pem(),
-        cert: cert.serialize_pem_with_signer(ca_cert)?,
-    })
+    Certificate::from_params(params).map_err(anyhow::Error::msg)
+}
+
+/// Gets a CA cert for signing purposes from a PEM file.
+///
+/// # Errors
+///
+/// The `ca_pem` or `ca_key_pem` are invalid.
+pub fn get_ca_cert(ca_pem: &str, ca_key_pem: &str) -> anyhow::Result<Certificate> {
+    let ca_keypair = KeyPair::from_pem(ca_key_pem)?;
+    let params = CertificateParams::from_ca_cert_pem(ca_pem, ca_keypair)?;
+
+    Certificate::from_params(params).map_err(anyhow::Error::msg)
 }
 
 #[cfg(test)]
@@ -69,16 +79,57 @@ mod tests {
         std::fs::write("rootCA.pem", cert.serialize_pem().unwrap()).ok();
         std::fs::write("rootCA-key.pem", cert.serialize_private_key_pem()).ok();
 
-        let kp = KeyPair::from_pem(&cert.get_key_pair().serialize_pem()).unwrap();
         let client_cert = create_client_cert(
-            &cert,
-            kp,
             "Test User 1".to_owned(),
             "_scpUz182381+hs@student.host.domain".to_owned(),
         )
         .unwrap();
 
-        std::fs::write("client-cert.pem", client_cert.cert).ok();
-        std::fs::write("client-key.pem", client_cert.key).ok();
+        std::fs::write(
+            "client-cert.pem",
+            client_cert.serialize_pem_with_signer(&cert).unwrap(),
+        )
+        .ok();
+        std::fs::write("client-key.pem", client_cert.serialize_private_key_pem()).ok();
+
+        let pfx = p12::PFX::new(
+            &client_cert.serialize_der_with_signer(&cert).unwrap(),
+            &client_cert.serialize_private_key_der(),
+            None,
+            "test-password",
+            "user 1",
+        );
+
+        std::fs::write("certs.pfx", pfx.unwrap().to_der()).ok();
+    }
+
+    #[test]
+    fn it_creates_a_client_cert() {
+        let ca_pem = std::fs::read_to_string("rootCA.pem").unwrap();
+        let ca_key_pem = std::fs::read_to_string("rootCA-key.pem").unwrap();
+        let ca_cert = get_ca_cert(&ca_pem, &ca_key_pem).unwrap();
+
+        let client_cert = create_client_cert(
+            "Test User 2".to_owned(),
+            "_scpUz000000+hs@student.host.domain".to_owned(),
+        )
+        .unwrap();
+
+        std::fs::write(
+            "client-cert.pem",
+            client_cert.serialize_pem_with_signer(&ca_cert).unwrap(),
+        )
+        .ok();
+        std::fs::write("client-key.pem", client_cert.serialize_private_key_pem()).ok();
+
+        let pfx = p12::PFX::new(
+            &client_cert.serialize_der_with_signer(&ca_cert).unwrap(),
+            &client_cert.serialize_private_key_der(),
+            None,
+            "test-password",
+            "Test User 2",
+        );
+
+        std::fs::write("certs.pfx", pfx.unwrap().to_der()).ok();
     }
 }
