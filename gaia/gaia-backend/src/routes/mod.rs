25a5e5d336f41b09a1d51632fd9c8a2c8d0cab7f
Jared L <48422312+lhjt@users.noreply.github.com>
2022-05-25 03:50:44 +1000

    chore(gaia): refactor to use `map_err`

--- gaia/gaia-backend/src/routes/mod.rs
+++ gaia/gaia-backend/src/routes/mod.rs
@@ -1,31 +1,31 @@
-use actix_web::{error::ErrorInternalServerError, get, Error, HttpRequest, HttpResponse};
+use actix_web::{
+    error::{ErrorBadRequest, ErrorInternalServerError, ErrorUnauthorized},
+    get, Error, HttpRequest, HttpResponse,
+};
 
 #[get("/roles")]
 pub(crate) async fn get_roles(req: HttpRequest) -> Result<HttpResponse, Error> {
     // Get JWT
-    let jwt_str = match req.headers().get("x-scp-auth") {
-        Some(value) => value.to_str().map_err(ErrorInternalServerError)?,
-        None => return Ok(HttpResponse::Unauthorized().body("Missing authentication token")),
-    };
+    let jwt_str = req
+        .headers()
+        .get("x-scp-auth")
+        .ok_or_else(|| ErrorUnauthorized("Missing authentication token"))?
+        .to_str()
+        .map_err(ErrorInternalServerError)?;
 
     // Validate str
-    let claims = match intra_jwt::verify_jwt(jwt_str, todo!()) {
-        Ok(claims) => claims,
-        Err(e) => return Ok(HttpResponse::BadRequest().body("Invalid authentication token")),
-    };
+    let claims = intra_jwt::verify_jwt(jwt_str, todo!())
+        .map_err(|_| ErrorBadRequest("Invalid authentication token"))?;
 
     let email = claims.user_id;
 
     // Get user id from email
-    let id = match email
+    let id = email
         .split('+')
         .next()
         .ok_or_else(|| ErrorInternalServerError("Internal server error: EC.SBP"))?
         .strip_prefix("_scpU")
-    {
-        Some(id) => id,
-        None => return Ok(HttpResponse::BadRequest().body("Invalid authentication token")),
-    };
+        .ok_or_else(|| ErrorBadRequest("Invalid authentication token"))?;
 
     // Get roles for id
     let roles: Vec<String> = todo!();
