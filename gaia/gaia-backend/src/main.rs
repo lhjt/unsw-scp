c63e8e98c85eb6e3f2e5caad2aa87a1b7c44db0e
Jared L <48422312+lhjt@users.noreply.github.com>
2022-05-25 17:07:42 +1000

    feat(gaia): add basic role fetching endpoint

--- gaia/gaia-backend/src/main.rs
+++ gaia/gaia-backend/src/main.rs
@@ -1,4 +1,7 @@
+use actix_web::{web::Data, App, HttpServer};
+use anyhow::Context;
 use migration::{Migrator, MigratorTrait};
+use routes::get_roles;
 
 mod routes;
 
@@ -12,5 +15,13 @@ async fn main() -> anyhow::Result<()> {
     let connection = sea_orm::Database::connect("sqlite://./db.db").await?;
     Migrator::up(&connection, None).await?;
 
-    Ok(())
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(connection.clone()))
+            .service(get_roles)
+    })
+    .bind(("0.0.0.0", 8081))?
+    .run()
+    .await
+    .context("failed to run and bind the server")
 }
--- gaia/gaia-backend/src/routes/mod.rs
+++ gaia/gaia-backend/src/routes/mod.rs
@@ -1,10 +1,15 @@
 use actix_web::{
     error::{ErrorBadRequest, ErrorInternalServerError, ErrorUnauthorized},
-    get, Error, HttpRequest, HttpResponse,
+    get, web, Error, HttpRequest, HttpResponse,
 };
+use entity::role;
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 
 #[get("/roles")]
-pub(crate) async fn get_roles(req: HttpRequest) -> Result<HttpResponse, Error> {
+pub(crate) async fn get_roles(
+    req: HttpRequest,
+    conn: web::Data<DatabaseConnection>,
+) -> Result<HttpResponse, Error> {
     // Get JWT
     let jwt_str = req
         .headers()
@@ -14,8 +19,11 @@ pub(crate) async fn get_roles(req: HttpRequest) -> Result<HttpResponse, Error> {
         .map_err(ErrorInternalServerError)?;
 
     // Validate str
-    let claims = intra_jwt::verify_jwt(jwt_str, todo!())
-        .map_err(|_| ErrorBadRequest("Invalid authentication token"))?;
+    let claims = intra_jwt::verify_jwt(
+        jwt_str,
+        &std::fs::read_to_string("../../proxy/certs/jwt-key.pem").unwrap(),
+    )
+    .map_err(|_| ErrorBadRequest("Invalid authentication token"))?;
 
     let email = claims.user_id;
 
@@ -23,12 +31,19 @@ pub(crate) async fn get_roles(req: HttpRequest) -> Result<HttpResponse, Error> {
     let id = email
         .split('+')
         .next()
-        .ok_or_else(|| ErrorInternalServerError("Internal server error: EC.SBP"))?
+        .ok_or_else(|| ErrorBadRequest("Invalid authentication token"))?
         .strip_prefix("_scpU")
         .ok_or_else(|| ErrorBadRequest("Invalid authentication token"))?;
 
     // Get roles for id
-    let roles: Vec<String> = todo!();
+    let roles: Vec<String> = role::Entity::find()
+        .filter(role::Column::UserId.eq(id))
+        .all(conn.into_inner().as_ref())
+        .await
+        .map_err(ErrorInternalServerError)?
+        .iter()
+        .map(|r| r.name.to_owned())
+        .collect();
 
     Ok(HttpResponse::Ok().json(roles))
 }
